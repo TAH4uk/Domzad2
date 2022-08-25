2749cf5cb9e5e8c7e11b1b0a5d4690e27b6b104e
TAH4uk <pcs_zero@inbox.ru>
2022-08-25 21:48:10 +0300

    domzad2

--- Program.cs
+++ Program.cs
@@ -30,20 +30,34 @@
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.Write("Введите номер дня недели: ");
+// Console.Write("Введите номер дня недели: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// while (number < 1 | number > 7)
+// {
+//     Console.Write($"Дня недели с номером {number} не существует. Введите номер дня недели: ");
+//     number = Convert.ToInt32(Console.ReadLine());
+// }
+
+// if (number >= 1 & number <= 5)
+// {
+//     Console.Write($"День недели с номером {number} является будним днем");
+// }
+// else if (number == 6 | number == 7)
+//     {
+//        Console.Write($"День недели с номером {number} является выходным днем"); 
+//     }
+
+// Дополнительная задача. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-while (number < 1 | number > 7)
+if ((number % 7 == 0) & (number % 23 == 0))
 {
-    Console.Write($"Дня недели с номером {number} не существует. Введите номер дня недели: ");
-    number = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Число {number} является кратным числам 7 и 23 одновременно");
 }
-
-if (number >= 1 & number <= 5)
+else
 {
-    Console.Write($"День недели с номером {number} является будним днем");
+    Console.Write($"Число {number} не является кратным числам 7 и 23 одновременно");
 }
-else if (number == 6 | number == 7)
-    {
-       Console.Write($"День недели с номером {number} является выходным днем"); 
-    }
